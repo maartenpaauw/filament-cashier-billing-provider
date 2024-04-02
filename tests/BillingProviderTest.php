358d4c2e034ab0c33cd5819958ea21ef4618b36c
Maarten Paauw <maartenpaauw@gmail.com>
2024-04-02 15:23:06 +0200

    refactor: chain pest test

--- tests/BillingProviderTest.php
+++ tests/BillingProviderTest.php
@@ -5,25 +5,13 @@ declare(strict_types=1);
 use Maartenpaauw\Filament\Cashier\Stripe\BillingProvider;
 
 it('should use the defined plan', function () {
-    // Arrange
-    $billingProvider = new BillingProvider('basic');
-
-    // Act
-    $middleware = $billingProvider->getSubscribedMiddleware();
-
-    // Assert
-    expect($middleware)
+    expect(new BillingProvider('basic'))
+        ->getSubscribedMiddleware()
         ->toEqual('Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed:basic');
 });
 
 it('should use the default plan when no plan provided', function () {
-    // Arrange
-    $billingProvider = new BillingProvider();
-
-    // Act
-    $middleware = $billingProvider->getSubscribedMiddleware();
-
-    // Assert
-    expect($middleware)
+    expect(new BillingProvider())
+        ->getSubscribedMiddleware()
         ->toEqual('Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed:default');
 });
--- tests/RedirectIfUserNotSubscribedTest.php
+++ tests/RedirectIfUserNotSubscribedTest.php
@@ -5,19 +5,11 @@ declare(strict_types=1);
 use Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed;
 
 it('should add the plan as parameter', function () {
-    // Act
-    $middleware = RedirectIfUserNotSubscribed::plan('basic');
-
-    // Assert
-    expect($middleware)
+    expect(RedirectIfUserNotSubscribed::plan('basic'))
         ->toEqual('Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed:basic');
 });
 
 it('should use the default plan when no plan provided', function () {
-    // Act
-    $middleware = RedirectIfUserNotSubscribed::plan();
-
-    // Assert
-    expect($middleware)
+    expect(RedirectIfUserNotSubscribed::plan())
         ->toEqual('Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed:default');
 });
