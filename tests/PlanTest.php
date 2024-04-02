45f18de40159a41bddf98ce5a7b748a68a5f9b19
Maarten Paauw <maartenpaauw@gmail.com>
2024-04-02 15:23:21 +0200

    test: add plan unit test

---
+++ tests/PlanTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Config\Repository;
+use Maartenpaauw\Filament\Cashier\Plan;
+
+beforeEach(function () {
+    $this->config = [
+        'cashier' => [
+            'plans' => [
+                'default' => [
+                    'price_id' => 'price_Fxp5y8x0qjrm2jjk2nzuMpSF',
+                    'trial_days' => 14,
+                    'allow_promotion_codes' => true,
+                    'collect_tax_ids' => true,
+                ],
+                'basic' => [
+                    'price_id' => 'price_ruVAAh5dwyhwbtWIVQn0lUvc',
+                ],
+            ],
+        ],
+    ];
+});
+
+test('it resolves all values from configuration', function () {
+    expect(new Plan(new Repository($this->config), 'default'))
+        ->type()
+        ->toEqual('default')
+        ->priceId()
+        ->toEqual('price_Fxp5y8x0qjrm2jjk2nzuMpSF')
+        ->trialDays()
+        ->toEqual(14)
+        ->allowPromotionCodes()
+        ->toBeTrue()
+        ->collectTaxIds()
+        ->toBeTrue();
+});
+
+test('it resolves all required values from configuration', function () {
+    expect(new Plan(new Repository($this->config), 'basic'))
+        ->type()
+        ->toEqual('basic')
+        ->priceId()
+        ->toEqual('price_ruVAAh5dwyhwbtWIVQn0lUvc')
+        ->trialDays()
+        ->toBeFalse()
+        ->allowPromotionCodes()
+        ->toBeFalse()
+        ->collectTaxIds()
+        ->toBeFalse();
+});
+
+it('throws an exception when the plan could not be parsed from configuration', function () {
+    expect(new Plan(new Repository($this->config), 'premium'))->priceId();
+})->throws(InvalidArgumentException::class, 'Invalid plan configuration');
