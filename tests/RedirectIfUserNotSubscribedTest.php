d8ebd5f86a424e54959cee43c77952854e1e14aa
Maarten Paauw <github@paauw.dev>
2025-03-02 10:18:54 +0100

    chore: narrow return types

--- tests/RedirectIfUserNotSubscribedTest.php
+++ tests/RedirectIfUserNotSubscribedTest.php
@@ -5,22 +5,22 @@ declare(strict_types=1);
 use Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed;
 use Workbench\App\enums\Plan;
 
-it('should add the plan as parameter', function () {
+it('should add the plan as parameter', function (): void {
     expect(RedirectIfUserNotSubscribed::plan('basic'))
         ->toEqual('Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed:basic');
 });
 
-it('should use the default plan when no plan provided', function () {
+it('should use the default plan when no plan provided', function (): void {
     expect(RedirectIfUserNotSubscribed::plan())
         ->toEqual('Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed:default');
 });
 
-it('should add the string plans as parameter', function () {
+it('should add the string plans as parameter', function (): void {
     expect(RedirectIfUserNotSubscribed::plan('basic,advanced,premium'))
         ->toEqual('Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed:basic,advanced,premium');
 });
 
-it('should add the enum plans as parameter', function () {
+it('should add the enum plans as parameter', function (): void {
     expect(RedirectIfUserNotSubscribed::plan(Plan::cases()))
         ->toEqual('Maartenpaauw\Filament\Cashier\Stripe\RedirectIfUserNotSubscribed:basic,advanced,premium');
 });
